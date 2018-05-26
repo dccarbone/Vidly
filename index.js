d90e494f8b016288c3331bd68f57c5a1a163b928
Diego Cavichiolli Carbone <dccarbone.app@gmail.com>
2018-05-25 21:52:02 -0300

    Adding new files

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const courses = [
+    { id: 1, name: "Course 1"},
+    { id: 2, name: "Course 2"},
+    { id: 3, name: "Course 3"}
+
+];
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find((c) => {
+        return c.id === parseInt(req.params.id)
+    })
+
+    if(!course) res.status(404).send('The given id was not found')
+    res.send(course)
+})
+
+const port = process.env.VIDLY_PORT || 3000
+
+app.listen(port, () => {
+    console.log("Starting server on port "+port)
+})
